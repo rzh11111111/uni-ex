9d34f947a06b119c19ffe3e2a9e621a376753a84
rzh11111111 <294346639@qq.com>
2020-04-21 14:10:31 +0800

    拿到登录信息

--- pages/login2/login2.vue
+++ pages/login2/login2.vue
@@ -18,6 +18,32 @@
 		methods:{
 			getuserinfo(en){
 				console.log(en)
+				
+				if(!en.detail.iv){ //用户授权点了取消
+					uni.showToast({
+						title:'您取消了授权，登录失败',
+						icon:'none'
+					})
+					return false;
+				}
+				//获取code
+				uni.login({
+					provider:'weixin',
+					success:function(loginRes){
+						console.log(loginRes)
+						//获取sessionkey
+						uni.request({
+							url:'https://hoa.hcoder.net/xcxencode/?c=sk&appid=wxa492a534992cfb5a&secret=f3f0507acae14b85f35a60cd3995917d&code='+loginRes.code,//获取sessionkey接口
+							success:function(res3){
+								console.log(res3)
+								//拿到res3.data.openid和res3.data.session_key
+							}
+						})
+					},
+					fail:function(loginRes){
+						console.log(loginRes)
+					}
+				})
 			}
 		}
 		
